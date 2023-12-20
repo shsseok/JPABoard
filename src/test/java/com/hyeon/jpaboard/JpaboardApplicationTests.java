d689975c13cb0bea534337232c8266b2f5bc83c4
Hyeon Seok <tlatms8919@gmail.com>
2023-12-20 11:41:50 +0900

    first commit

---
+++ src/test/java/com/hyeon/jpaboard/JpaboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hyeon.jpaboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
